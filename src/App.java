656463cb1701f111800656f7c1417082750a26d8
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-05-28 16:59:54 +0300

    Rows, columns, width, and height added

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        //19 coloums, 21 rows, each tile 32x32 pixels
+        int rowCount = 21;
+        int columnCount = 19;
+        int tileSize = 32;
+        int boardwidth = columnCount*tileSize;
+        int boardHeight = rowCount*tileSize;
+
+        
+    }
+}
